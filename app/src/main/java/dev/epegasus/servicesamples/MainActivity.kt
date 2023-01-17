fbf867fa39c84cfdeee30468386b79025ca66865
Sohaib <sohaibahmed.hypersoft@gmail.com>
2023-01-17 12:15:35 +0500

    Services

---
+++ app/src/main/java/dev/epegasus/servicesamples/MainActivity.kt
@@ -0,0 +1,17 @@
+package dev.epegasus.servicesamples
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.epegasus.servicesamples.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+
+}
